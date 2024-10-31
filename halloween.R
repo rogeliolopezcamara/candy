c883292a49a8ab742bafcd908e5a30ffb578c394
morgantimewarp <51034455+morgantimewarp@users.noreply.github.com>
2024-10-31 17:14:38 -0400

    Update halloween.R
    added functionality to check if today is halloween 2024

--- halloween.R
+++ halloween.R
@@ -1,3 +1,10 @@
 # Today is Halloween
 Sys.Date()
 # Tomorrow will not be Halloween.
+
+# Script to check halloween
+if(Sys.Date() - as.Date("2024-10-31") == 0){
+  print("Today is Halloween!")
+}else{
+  "Unfortunately, today is not Halloween"
+}
